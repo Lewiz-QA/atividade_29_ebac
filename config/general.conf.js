9d95a28e0531ee647a4c1a38359339d1249e906b
Luiz <lgm97.com@gmail.com>
2023-12-11 00:00:47 -0300

    Aula 1 Módulo 30

---
+++ config/general.conf.js
@@ -0,0 +1,20 @@
+const { hooksConf } = require("./hooks.conf")
+const { reportersConf } = require("./reporters.conf")
+const { specsConf } = require("./specs.conf")
+
+let generalConf = {
+    path: '/wd/hub',
+    framework: 'mocha',
+    waitforTimeout: 20000,
+    mochaOpts: {
+        timeout: 300000
+    },
+    maxInstances: 1,
+    ...hooksConf,
+    ...reportersConf,
+    ...specsConf
+
+}
+
+
+module.exports = { generalConf }
---
+++ config/wdio.conf.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+function getConfig() {
+    switch (process.env.ENVIRONMENT) {
+        case 'local':default:
+            return localConf
+        case 'browserstack':
+            return {}
+        case 'saucelabs':
+            return {}
+    }
+}
+
+exports.config = getConfig()
