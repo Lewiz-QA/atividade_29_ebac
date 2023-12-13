75cd08f99d4da63f88788619764eb94f1dfb1d8f
Luiz <lgm97.com@gmail.com>
2023-12-12 23:58:34 -0300

    Aula 2 Módulo 30

--- config/bs.conf.js
+++ config/bs.conf.js
@@ -5,18 +5,16 @@ const { generalConf } = require('./general.conf')
 let capabilities = process.env.PLATFORM === 'android' ? {
     capabilities: [{ 
         app: `${process.env.ANDROID_APP_ID}`,
-        device: 'Samsung Galaxy Note 20' ,
+        device: 'Samsung Galaxy Note 20',
         os_version: '10.0',
         project: 'Meu primeiro projeto em Device Farm',
         build: 'EBAC CI Mobile',
         name: 'teste_login'
     }]
 } : {
-    capabilities: [{ 
+    capabilities: [{
         app: `${process.env.IOS_APP_ID}`,
-        device: 'Samsung Galaxy Note 20' ,
-        os_version: '10.0',
-        project: 'Meu primeiro projeto Appium iOS BS',
+        project: "Meu primeiro projeto Appium iOS BS",
         build: 'EBAC Test iOS',
         name: 'ebac_test',
         device: 'iPhone 12 Pro',
