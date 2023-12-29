125cdf848d64bd55a64994add3f370be84df1c6c
Luiz <lgm97.com@gmail.com>
2023-12-28 22:51:58 -0300

    Refatorações

--- test/specs/checkout.spec.js
+++ test/specs/checkout.spec.js
@@ -2,7 +2,7 @@ const checkoutViewScreen = require("../screens/checkoutView.screen");
 
 describe('Deve realizar Checkout corretamente', () => {
 
-    it('Deve alertar ao tentar realizar Checkout sem itens no Carrinho', async () => {
+    it('Deve alertar ao tentar acessar Checkout sem itens no Carrinho', async () => {
         await checkoutViewScreen.accessDrawer()
         await checkoutViewScreen.accessCart()
         await checkoutViewScreen.accessCheckout()
